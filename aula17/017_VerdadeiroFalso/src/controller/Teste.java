be329098f193dd198eb9d1b6006bb203ba3857f0
Gabriel Lima <44061888+GabrielL26@users.noreply.github.com>
2018-11-01 20:47:13 -0300

    Add files via upload

---
+++ aula17/017_VerdadeiroFalso/src/controller/Teste.java
@@ -0,0 +1,24 @@
+package controller;
+
+public class Teste {
+	
+	public static void main(String[] args) {
+		boolean b = false;
+		boolean a = true;
+		boolean c = a || b;
+		if(b) 
+		{
+			System.out.println(c);
+		}
+		/*if ( a != b ) {
+			System.out.println("Iguais");
+		}
+		else
+		{
+			System.out.println("Diferentes");
+		}*/
+		
+		
+	}
+
+}
